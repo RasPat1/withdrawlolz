add4567f35b5c3701e5c4fbf9df0f5f4903542e5
Rasesh Patel <Rasesh@shoptiques.com>
2015-11-27 10:55:09 -0500

    rename some vars

--- lottery/Lottery.java
+++ lottery/Lottery.java
@@ -15,16 +15,16 @@ class Lottery {
     System.out.println(blue);
   }
 
-  public static Long getTicketCount(Integer numberCount, Integer maxNumber, Boolean unique, Boolean nonDescending) {
+  public static Long getTicketCount(Integer blanks, Integer choices, Boolean unique, Boolean sorted) {
     Long ticketCount = 0L;
-    if (unique && nonDescending) {
-      ticketCount = getAscendingLottery(numberCount, maxNumber);
-    } else if (unique && !nonDescending) {
-      ticketCount = getNoDuplicateLottery(numberCount, maxNumber);
-    } else if (!unique && nonDescending) {
-      ticketCount = getNonDescendingLottery(numberCount, maxNumber);
+    if (unique && sorted) {
+      ticketCount = getAscendingLottery(blanks, choices);
+    } else if (unique && !sorted) {
+      ticketCount = getNoDuplicateLottery(blanks, choices);
+    } else if (!unique && sorted) {
+      ticketCount = getNonDescendingLottery(blanks, choices);
     } else {
-      ticketCount = getSimpleLottery(numberCount, maxNumber);
+      ticketCount = getSimpleLottery(blanks, choices);
     }
     return ticketCount;
   }
@@ -33,22 +33,22 @@ class Lottery {
    * Case 1
    * Lottery numbers are stictly ascending.  No duplicates.
   **/
-  public static Long getAscendingLottery(Integer numberCount, Integer maxNumber) {
+  public static Long getAscendingLottery(Integer blanks, Integer choices) {
     Long total = 0L;
 
-    if (numberCount > maxNumber) {
+    if (blanks > choices) {
       return 0L;
-    } else if (numberCount == maxNumber) {
+    } else if (blanks == choices) {
       return 1L;
     }
 
-    if (numberCount == 1) {
-	return (long) maxNumber;
+    if (blanks == 1) {
+	return (long) choices;
     }
 
-    for (int i = 0; i < maxNumber; i++) {
-      int newFloor = maxNumber - i;
-      total += getAscendingLottery(numberCount - 1, newFloor - 1);
+    for (int i = 0; i < choices; i++) {
+      int newFloor = choices - i;
+      total += getAscendingLottery(blanks - 1, newFloor - 1);
     }
 
     return total;
@@ -56,19 +56,19 @@ class Lottery {
 
   /**
    * Case 2
-   * Lottery ticket is composed of a set of `numberCount` numbers with a 
-   * range of 1 to `maxNumber` where numbers are in non descending order
+   * Lottery ticket is composed of a set of `blanks` numbers with a 
+   * range of 1 to `choices` where numbers are in non descending order
   **/
-  public static Long getNonDescendingLottery(Integer numberCount, Integer maxNumber) {
+  public static Long getNonDescendingLottery(Integer blanks, Integer choices) {
     Long total = 0L;
 
-    if (numberCount == 1) {
-      return 0L + maxNumber;
+    if (blanks == 1) {
+      return 0L + choices;
     }
 
-    for (int i = 0; i < maxNumber; i++) {
-      int newFloor = maxNumber - i;
-      total += getNonDescendingLottery(numberCount - 1, newFloor);
+    for (int i = 0; i < choices; i++) {
+      int newFloor = choices - i;
+      total += getNonDescendingLottery(blanks - 1, newFloor);
     }
 
     return total;
@@ -76,24 +76,24 @@ class Lottery {
 
   /** 
    * Case 3
-   * Lottery ticket is composed of a set of `numberCount` numbers with a 
-   * a range of 1 to `maxNumber` with no duplicate numbers
+   * Lottery ticket is composed of a set of `blanks` numbers with a 
+   * a range of 1 to `choices` with no duplicate numbers
   **/
-  public static Long getNoDuplicateLottery(Integer numberCount, Integer maxNumber) {
+  public static Long getNoDuplicateLottery(Integer blanks, Integer choices) {
     Long total = 1L;
-    for (int i = 0; i < numberCount; i++) {
-      total = total * (numberCount - i);
+    for (int i = 0; i < blanks; i++) {
+      total = total * (blanks - i);
     } 
     return total;
   }
 
   /** 
    * Case 4
-   * Lottery ticket is composed of a set of `numberCount` numbers with
-   * a range of 1 to `maxNumber`
+   * Lottery ticket is composed of a set of `blanks` numbers with
+   * a range of 1 to `choices`
    **/
-  public static Long getSimpleLottery(Integer numberCount, Integer maxNumber) {
-    return (long) Math.pow(maxNumber, numberCount);
+  public static Long getSimpleLottery(Integer blanks, Integer choices) {
+    return (long) Math.pow(choices, blanks);
   }
 
 }
