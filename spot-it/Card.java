a8cd09fd981b8267d51ce676037857d902995cc6
Rasesh Patel <Rasesh@shoptiques.com>
2015-12-29 02:15:10 -0500

    Create a valid deck using random brute force

--- spot-it/Card.java
+++ spot-it/Card.java
@@ -3,6 +3,7 @@ import java.util.Random;
 class Card {
   Integer id;
   CardImage[] images;
+  Integer imageCount = 0;
 
   static Integer deckSize = 0;
   static Integer defaultSlots = 4;
@@ -11,13 +12,17 @@ class Card {
     deckSize++;
     this.id = deckSize;
     this.images = new CardImage[4];
+    this.imageCount = 0;
   }
 
   public static Card getRandomCard() {
     Card card = new Card();
 
     for (int i = 0; i < defaultSlots; i++) {
-      card.addImage(CardImage.getRandomImage());
+      Boolean added = false;
+      while (!added) {
+        added = card.addImage(CardImage.getRandomImage());
+      }
     }
 
     return card;
@@ -27,13 +32,30 @@ class Card {
    * Add an image to the card at the first empty spot
    *
   **/
-  public void addImage(CardImage image) {
-    for (int i = 0; i < images.length; i++) {
-      if (images[i] == null) {
-        images[i] = image;
-        break;
+  public Boolean addImage(CardImage image) {
+    Boolean added = false;
+
+    if (containsImage(image)) {
+      added = false;
+    } else {
+      images[imageCount] = image;
+      added = true;
+      imageCount++;
+    }
+
+    return added;
+  }
+
+  public Boolean containsImage(CardImage image) {
+    Boolean hasImage = false;
+
+    for (int i = 0; i < this.imageCount; i++) {
+      if (image.isEqual(this.images[i])) {
+        hasImage = true;
       }
     }
+
+    return hasImage;
   }
 
   public String toString() {
--- spot-it/Spot.java
+++ spot-it/Spot.java
@@ -5,20 +5,47 @@
 
 class Spot {
   public static void  main(String args[]) {
-    createDeck();
+    Long startTime = System.currentTimeMillis();
+
+    Card[] goodDeck = createValidDeck(8);
+    Boolean isGoodDeck = testDeck(goodDeck);
+
+    Long endTime = System.currentTimeMillis();
+
+    System.out.println("Deck built in: " + (endTime - startTime));
+
+    if (isGoodDeck) {
+      System.out.println("Deck is valid");
+    } else {
+      System.out.println("Deck is invalid");
+    }
+
+    for (int i = 0; i < goodDeck.length; i++) {
+      System.out.println(goodDeck[i]);
+    }
+
   }
 
-  public static void createDeck() {
+  public static Card[] createValidDeck(int deckSize) {
+    Boolean isGoodDeck = false;
+    Card[] deck = null;
 
-    Card[] deck = new Card[10];
+    while (!isGoodDeck) {
+      deck = createDeck(deckSize);
+      isGoodDeck = testDeck(deck);
+    }
+
+    return deck;
+  }
+
+  public static Card[] createDeck(int deckSize) {
+
+    Card[] deck = new Card[deckSize];
     for (int i = 0; i < deck.length; i++) {
       deck[i] = Card.getRandomCard();
     }
 
-    for (int i = 0; i < deck.length; i++) {
-      System.out.println(deck[i]);
-    }
-
+    return deck;
   }
 
   /**
@@ -27,11 +54,11 @@ class Spot {
    * only one common image exists for each pair
    *
    **/
-  Boolean testDeck(Card[] deck) {
+  static Boolean testDeck(Card[] deck) {
     Boolean isGoodDeck = true;
 
     for (int i = 0; i < deck.length - 1; i++) {
-      for (int j = i; j < deck.length; j++) {
+      for (int j = i + 1; j < deck.length; j++) {
         Card card1 = deck[i];
         Card card2 = deck[j];
         isGoodDeck = isGoodDeck && testPair(card1, card2);
@@ -49,7 +76,7 @@ class Spot {
    * the pair of cards
    *
    **/
-  Boolean testPair(Card card1, Card card2) {
+  static Boolean testPair(Card card1, Card card2) {
     int matchCount = 0;
 
     for (int i = 0; i < card1.images.length; i++) {
