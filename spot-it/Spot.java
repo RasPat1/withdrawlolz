d407176e5ef296b140222d1d3fb02495ec211fb9
Rasesh Patel <Rasesh@shoptiques.com>
2015-12-29 04:58:03 -0500

    Benchmark random brute force approach

--- spot-it/Spot.java
+++ spot-it/Spot.java
@@ -3,42 +3,86 @@
  *
 **/
 
+import java.util.ArrayList;
+
 class Spot {
   public static void  main(String args[]) {
-    Stat stat = new Stat(6, 5);
-    Deck goodDeck = createValidDeck(6, 5, stat);
-
-    System.out.println(stat);
-    System.out.println(goodDeck);
+    testingSuite();
   }
 
 
   /**
   *  Try generating several different combinations of deckSizes and
   *  imagesPerCard and gathering statistics on how difficult it is
-  *  Important metrics: "Time taken, Decks generated, Cards Generated"
+  *  Important metrics: Time taken, Decks generated, Cards Generated
   *
   **/
   public static void testingSuite() {
-    // Stat stat = new Stat(deckSize, imagesPerCard);
-    // stat.start();
-    // Deck goodDeck = createValidDeck(deckSize, imagesPerCard, stat);
-    // stat.end();
+    ArrayList<Stat> stats = new ArrayList<Stat>();
+
+    testWrapper(6, 4, 5, stats);
+    // testWrapper(6, 5, 5, stats);
+    // testWrapper(6, 6, 5, stats);
+    // testWrapper(7, 4, 2, stats);
+    // testWrapper(7, 5, 5, stats);
+    // testWrapper(7, 6, 5, stats);
+    // testWrapper(7, 7, 5, stats);
+    // testWrapper(8, 4, 5, stats);
+    // testWrapper(8, 5, 5, stats);
+    // testWrapper(8, 6, 5, stats);
+    // testWrapper(8, 7, 5, stats);
+    // testWrapper(8, 8, 5, stats);
+    // testWrapper(9, 5, 5, stats);
+    // testWrapper(9, 7, 5, stats);
+    // testWrapper(9, 9, 5, stats);
+
+    Long totalTimeTaken = 0L;
+    Long totalDecksBuilt = 0L;
+    Long totalCardsBuilt = 0L;
+    for (int i = 0; i < stats.size(); i++) {
+      Stat stat = stats.get(i);
+      totalTimeTaken += stat.timeTaken;
+      totalDecksBuilt += stat.decksBuilt;
+      totalCardsBuilt += stat.cardsBuilt;
+    }
+
+    System.out.println("totalCardsBuilt:" + totalCardsBuilt);
+    System.out.println("totalDecksBuilt:" + totalDecksBuilt);
+    System.out.println("totalTimeTaken:" + totalTimeTaken);
+
   }
 
-  public static Deck createValidDeck(int deckSize, int imagesPerCard, Stat stat) {
+  public static void testWrapper(int deckSize, int imagesPerCard,
+                                int trials, ArrayList<Stat> stats) {
+    Stat stat = new Stat(deckSize, imagesPerCard);
+
+    for (int i = 0; i < trials; i++) {
+      Deck goodDeck = createValidDeck(deckSize, imagesPerCard, stat);
+    }
+
+    System.out.println(stat);
+    stats.add(stat);
+  }
+
+  public static Deck createValidDeck(int deckSize, int imagesPerCard,
+                                     Stat stat) {
     Boolean isGoodDeck = false;
     Deck deck = null;
+    Long bailOutCount = (long) Math.pow(10, 8);
 
     stat.start();
 
-    while (!isGoodDeck) {
+    while (!isGoodDeck && stat.decksBuilt < bailOutCount) {
       deck = Deck.createRandomDeck(deckSize, imagesPerCard, stat);
       isGoodDeck = testDeck(deck);
       stat.deckBuilt();
     }
 
     stat.end();
+    if (isGoodDeck) {
+      stat.successfulTrial();
+    }
+
 
     return deck;
   }
--- spot-it/Stat.java
+++ spot-it/Stat.java
@@ -9,21 +9,28 @@ class Stat {
   Long timeStart;
   Long timeEnd;
 
+  Integer trials = 0;
+  Integer successfulTrials = 0;
+
   public Stat(int deckSize, int imagesPerCard) {
     decksBuilt = 0L;
     cardsBuilt = 0L;
     timeTaken = 0L;
+
     this.deckSize = deckSize;
     this.imagesPerCard = imagesPerCard;
   }
 
   public String getStats() {
     StringBuffer statString = new StringBuffer();
+
     statString.append("deckSize:" + deckSize + "\n");
     statString.append("imagesPerCard:" + imagesPerCard + "\n");
-    statString.append("timeTaken:" + timeTaken + "\n");
-    statString.append("decksBuilt:" + decksBuilt + "\n");
-    statString.append("cardsBuilt:" + cardsBuilt + "\n");
+    statString.append("timeTaken:" + timeTaken/trials + "\n");
+    statString.append("decksBuilt:" + decksBuilt/trials + "\n");
+    statString.append("cardsBuilt:" + cardsBuilt/trials + "\n");
+    statString.append("trials:" + trials + "\n");
+    statString.append("successfulTrials:" + successfulTrials + "\n");
 
     // Time per Deck
     // Time per Card
@@ -42,11 +49,16 @@ class Stat {
 
   public void start() {
     timeStart = System.currentTimeMillis();
+    trials++;
   }
 
   public void end() {
     timeEnd = System.currentTimeMillis();
-    timeTaken = timeEnd - timeStart;
+    timeTaken += (timeEnd - timeStart);
+  }
+
+  public void successfulTrial() {
+    successfulTrials++;
   }
 
   public String toString() {
