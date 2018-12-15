9660093513bc7e587f38da04c4a222383307abdf
Rasesh Patel <raspat1@gmail.com>
2018-12-15 02:40:15 -0500

    Make graphs prettier

--- narcs/lib/ResultHolder.rb
+++ narcs/lib/ResultHolder.rb
@@ -4,7 +4,7 @@ require 'fileutils'
 class ResultHolder
   attr_accessor :name, :results
   FILE_OUTPUT_TYPE = 'png'
-  PATH_PREFIX = './outputs1'
+  PATH_PREFIX = './outputs'
 
   def initialize(name)
     @name = name
@@ -21,34 +21,48 @@ class ResultHolder
     end
   end
 
-  # figure out how chart the results here
+  # Create a file with some visual representation of this set of results
   def print_chart
     g = Gruff::Line.new
+
     g.title = name
+    g.y_axis_label = "Time (ms)"
+    g.x_axis_label = "Solutions Found"
+    g.hide_dots = true
+
+    labels = {}
+    1.upto(50) { |val| labels[val] = val.to_s }
+    g.labels = labels
+
     results.each do |result|
-      g.dataxy(result.name, result.data.map { |solution| [solution.value, solution.elapsed_time] })
+      xy_coords = result.data.map.with_index(1) { |solution, index| [index, solution.elapsed_time * 1000] }
+      g.dataxy(result.name, xy_coords)
     end
 
     g.write(output_file_name)
   end
 
   def output_file_name
-    file_name = sanitize_filename(name) + Time.now.to_i.to_s
+    # file_name = sanitize_filename(name) + '-' + Time.now.to_i.to_s
+    file_name = sanitize_filename(name)
     FileUtils.mkdir_p(PATH_PREFIX)
 
     "#{PATH_PREFIX}/#{file_name}.#{FILE_OUTPUT_TYPE}"
   end
 
   # https://stackoverflow.com/questions/1939333/how-to-make-a-ruby-string-safe-for-a-filesystem
-  def sanitize_filename(filename)
-    filename.strip do |name|
-     # NOTE: File.basename doesn't work right with Windows paths on Unix
-     # get only the filename, not the whole path
-     name.gsub!(/^.*(\\|\/)/, '')
+  def sanitize_filename(file_name)
+   # NOTE: File.basename doesn't work right with Windows paths on Unix
+   # get only the file_name, not the whole path
+   file_name = file_name.gsub(/^.*(\\|\/)/, '')
 
-     # Strip out the non-ascii character
-     name.gsub!(/[^0-9A-Za-z.\-]/, '_')
-    end
+   # Strip out the non-ascii character
+   file_name = file_name.gsub(/[^0-9A-Za-z.\-]/, '_')
+
+   file_name = file_name.downcase
+   file_name = file_name.gsub(' ', '_')
+
+   file_name
   end
 end
 
