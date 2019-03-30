d082a9886865f8d5e48f709758fedd8f24b95d47
Rasesh Patel <raspat1@gmail.com>
2019-03-29 20:23:26 -0400

    Adding some bits that i'm writing while watching OCW lectures

---
+++ misc/MIT/6.006/PeakFinder2D.rb
@@ -0,0 +1,13 @@
+# A 2D peak finder
+# Given a 2D list of integers return the location
+# of a peak if one exists
+# A 2D peak is a location i,j such that the
+# value at i,j is greater than or equal to each
+# of the values (i+1,j), (i-1,j), (i,j-1), (i,j+1)
+# if they exist
+# There are a number of different algorithms
+# What can we try?
+class PeakFinder2D
+  def find
+  end
+end
