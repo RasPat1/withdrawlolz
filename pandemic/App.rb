cc52b8d18970d9d75b3896bfb0b3dfbd66d909a2
Rasesh Patel <raspat1@gmail.com>
2020-10-16 22:27:32 -0600

    Add a basic renderer for showing the game state.
    I just wanted tot check tthat this silly thing woudl work.

--- pandemic/App.rb
+++ pandemic/App.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 require './lib/Pandemic.rb'
+require './gui/Renderer.rb'
+
+also_reload './lib/Pandemic.rb'
+also_reload './gui/Renderer.rb'
 
 get '/start' do
   game = Pandemic.new
+  HTMLRenderer.new(game).render
 end
---
+++ pandemic/gui/Renderer.rb
@@ -0,0 +1,30 @@
+# The renderer accepts a game object and creates a visual representation of that.
+
+# Dev Paths
+# Add react to the framework and render the game object as json and send that to an indpendent frontend
+# Create a CLI renderer that you can use for the local version of this
+# Make a crazy basic ruby server side render html css string that gets pumped to the output.  This is so weird and basic.  We'll start this way for fun.
+
+# Also, one of the real issues here is that the game engign is sending user messages and collecting data from the terminal. We want a more robust way of io.
+
+class Renderer
+  attr_accessor :game
+  def initialize(game)
+    @game = game
+  end
+end
+
+# Push out an html/css page
+class HTMLRenderer < Renderer
+  def render
+    output = ""
+    output += "<h1>Pandemic</h1>"
+    output += "<p>#{game}</p>"
+    return output
+  end
+end
+
+
+
+# Should the game be passed to the renderer or the renderer to be passed to the game?
+# THe game object right now hides all of it's data but exposes a to_s method. It also is printing shit on it's own. The printing should be somewhere else no?
